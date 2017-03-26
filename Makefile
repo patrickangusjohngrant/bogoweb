ba66c7a92cd4c84192c72df75c95d9ed15874a43
Patrick Grant <patrick@inverbrough.co.uk>
2017-03-26 11:26:17 +0100

    Odds and ends
    - Create bogoCA.
    - Increase file descriptors
    - Mutex to allow cooperate between async and lwt :s
    - Other bugfixes

--- Makefile
+++ Makefile
@@ -2,17 +2,45 @@ all: bogoweb
 
 bogoweb: bogoweb.ml
 	ocamlfind ocamlopt bogoweb.ml -package lwt,dns.lwt,core,cohttp.async,async -thread -linkpkg -g -o bogoweb
-	sudo chown root bogoweb
-	sudo chmod +s bogoweb
+
+bogoca_dir:
+	mkdir -p /tmp/bogoCA/newcerts
+
+/tmp/bogoCA/index.txt.attr:
+	touch /tmp/bogoCA/index.txt.attr /tmp/bogoCA/index.txt
+
+/tmp/bogoCA/serial:
+	echo -n 1111 | tee /tmp/bogoCA/serial
+
+/tmp/bogoCA/ca.key.pem: bogoca_dir
+	openssl genrsa -out /tmp/bogoCA/ca.key.pem 512
+
+/tmp/bogoCA/ca.cert.pem: bogoca_dir /tmp/bogoCA/ca.key.pem
+	openssl req \
+		-subj "/C=GB/ST=bogoweb/L=bogoweb/O=bogoweb/OU=Bogoweb Department/CN=Bogoweb CA" \
+		-key /tmp/bogoCA/ca.key.pem \
+		-new -x509 \
+		-days 7300 \
+		-sha256 \
+		-extensions v3_ca \
+		-out /tmp/bogoCA/ca.cert.pem
+
+bogoca: /tmp/bogoCA/ca.cert.pem /tmp/bogoCA/ca.key.pem /tmp/bogoCA/serial /tmp/bogoCA/index.txt.attr
+	sudo mkdir -p  /usr/share/ca-certificates/extra
+	sudo openssl x509 -in /tmp/bogoCA/ca.cert.pem -inform PEM -out /usr/share/ca-certificates/extra/bogoca.crt
+	sudo dpkg-reconfigure ca-certificates
 
 tidyips:
 	for i in $(shell ip address | egrep lo$$ | grep -v 127.0 | awk '{print $$2}') ; do sudo ip address del $$i dev lo ; done
 
-run: bogoweb tidyips
+run: bogoweb tidyips bogoca
 	echo nameserver 127.0.0.1 | sudo tee /etc/resolv.conf
-	./bogoweb
+	sudo ./bogoweb
 
 clean: tidyips
 	echo nameserver 127.0.1.1 | sudo tee /etc/resolv.conf
 	rm -f bogoweb bogoweb.cmi bogoweb.cmx bogoweb.o
 	sudo rm -rf /tmp/certs
+
+veryclean: clean
+	rm -r /tmp/bogoCA
--- bogoweb.ml
+++ bogoweb.ml
@@ -1,28 +1,80 @@
 (* Forwarding DNS server example. Looks up query locally first then forwards to another resolver. *)
 open Lwt
 open Dns
-open Core.Std
+open Core.Std;;
 
 let lookup_table = Hashtbl.create ~hashable:String.hashable ();;
 
+let random_status () = match Random.int 10 with
+  | 0     -> `Unauthorized
+  | 1     -> `Not_found
+  | 2     -> `Forbidden
+  | _ -> `OK
+
+(* TODO: make this do more random stuff:
+ - sleep
+ - fail
+ - fail with different things (403, 401, 500, etc)
+*)
 let http_server ~body _ req =
   let open Async.Std in
   let open Cohttp in
   let open Cohttp_async in
-  match req |> Cohttp.Request.meth with
-  | `POST ->
-        (Body.to_string body) >>= (fun body ->
-          Log.Global.info "Body: %s" body;
-          Server.respond `OK)
-  | `GET -> Server.respond_string ~status:`OK (String.make (Random.int 1000) 'a')
-  | _ -> Server.respond `Method_not_allowed;;
+  Async.Std.after (sec (Random.float 3.0)) >>=
+  fun () -> Server.respond_string ~status:(random_status ()) (String.make (Random.int 1000) 'a');;
 
-let http_listener name ip =
+let generate_certs_sequencer = Async.Std.Throttle.Sequencer.create ();;
+
+let generate_certs name =
+  let open Async.Std in
+  let cert_dir = "/tmp/certs/" ^ name
+  in
+  let key_path = cert_dir ^ "/key.out"
+  in
+  let csr_path = cert_dir ^ "/csr.out"
+  in
+  let cert_path = cert_dir ^ "/cert.out"
+  in
+  Unix.mkdir ~p:() cert_dir
+  >>= fun _ -> Async_unix.Process.create_exn
+    ~prog:"/usr/bin/openssl"
+    ~args:[
+      "req";
+      "-nodes";
+      "-newkey"; "rsa:512";
+      "-keyout"; key_path;
+      "-out"; csr_path;
+      "-subj";
+      "/C=GB/ST=bogoweb/L=bogoweb/O=bogoweb/OU=Bogoweb Department/CN="^name]
+      ()
+  >>= Async_unix.Process.wait
+  >>= (fun _ -> Throttle.enqueue generate_certs_sequencer (Async_unix.Process.create_exn
+    ~prog:"/usr/bin/openssl"
+    ~args:[
+      "ca";
+      "-batch";
+      "-config";
+      "./openssl.cnf";
+      "-keyfile"; "/tmp/bogoCA/ca.key.pem";
+      "-cert"; "/tmp/bogoCA/ca.cert.pem";
+      "-days"; "375";
+      "-notext";
+      "-md"; "sha256";
+      "-in"; csr_path;
+      "-out"; cert_path;]
+      ))
+  >>= Async_unix.Process.wait
+  >>= fun _ -> return (key_path, cert_path)
+
+let http_listener name ip mutex =
   let open Async.Std in
   let open Async_extra.Tcp in
   let open Cohttp in
   let open Cohttp_async in
-  let cert_dir = "/tmp/certs/" ^ name
+  let error_logger = (`Call (fun addr exn ->
+    Logs.err (fun f -> f "Error from %s" (Socket.Address.to_string addr));
+    Logs.err (fun f -> f "%s" @@ Exn.to_string exn))
+  )
   in
   let socket_builder port =
     Where_to_listen.create
@@ -36,32 +88,26 @@ let http_listener name ip =
     ~prog:"/sbin/ip"
     ~args:["address"; "add"; Ipaddr.V4.to_string ip; "dev"; "lo"] ()
   >>= Async_unix.Process.wait
-  >>= fun _ -> Unix.mkdir ~p:() cert_dir
-  >>= fun _ -> Async_unix.Process.create_exn
-    ~prog:"/usr/bin/openssl"
-    ~args:[
-      "req";
-      "-nodes";
-      "-newkey"; "rsa:512";
-      "-keyout"; cert_dir ^ "/key.out";
-      "-out"; cert_dir ^ "/csr.out";
-      "-subj";
-      "/C=GB/ST=patrick/L=patrick/O=patrick/OU=Patrick Department/CN="^name]
-      ()
-  >>= fun _ ->
+  >>= fun _ -> generate_certs name
+  >>= fun (key_path, cert_path) ->
   Server.create
     ~mode:(`OpenSSL (
-        `Crt_file_path "/etc/ssl/certs/ssl-cert-snakeoil.pem",
-        `Key_file_path "/etc/ssl/private/ssl-cert-snakeoil.key"
+        `Crt_file_path cert_path,
+        `Key_file_path key_path
     ))
+    ~on_handler_error:error_logger
     (socket_builder 443)
     http_server
   >>= fun _ ->
   Server.create
     ~mode:`TCP
+    ~on_handler_error:error_logger
     (socket_builder 80)
     http_server
-  >>= fun _ -> Deferred.never ();;
+  >>= fun _ -> (
+    Lwt_mutex.unlock mutex;
+    Deferred.never ()
+  );;
 
 
 let empty_response = {
@@ -75,9 +121,12 @@ let empty_response = {
 let generator name =
   let ip = (Ipaddr.V4.of_int32 (Random.int32 Int32.max_value))
   in
-  let () = http_listener name ip |> ignore
-in
-{
+  let mutex = Lwt_mutex.create ()
+  in 
+  http_listener name ip mutex |> ignore;
+  Lwt_mutex.lock mutex >>=
+  fun () -> Lwt_mutex.lock mutex >>=
+  fun () -> return {
   empty_response with
   Dns.Query.answer=[{
     Dns.Packet.name=Dns.Name.of_string name;
@@ -95,11 +144,28 @@ let dns_process ~src ~dst packet =
       | [{q_name=query; q_type=Q_A; q_class=_; q_unicast=_}] ->
           let name = Name.to_string query 
           in
-          Hashtbl.find_or_add lookup_table name ~default:(fun () -> generator name) |> Option.some |> return
+          let contains_dots = String.contains name '.'
+          in
+          if not contains_dots then
+            return (Some empty_response)
+          else
+            Hashtbl.find_or_add
+              lookup_table
+              name
+              ~default:(fun () -> generator name) >>=
+            fun x -> (return (Some x))
       | [_] -> return (Some empty_response)
       | _::_::_ -> return None;;
 
-Thread.create Async.Std.Scheduler.go () |> ignore;;
+let max_fd = 1000000L;;
+
+(* Up ulimits. Bizarro types. *)
+Unix.RLimit.set Unix.RLimit.num_file_descriptors {
+  Core.Std.Unix.RLimit.cur = Unix.RLimit.Limit max_fd;
+  max = Unix.RLimit.Limit max_fd
+};
+
+Thread.create (Async.Std.Scheduler.go_main ~max_num_open_file_descrs:(Int.of_int64_exn max_fd) ~main:(fun () -> ())) ();;
 
 let () =
     Lwt_main.run (  
